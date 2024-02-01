7bc7c63f283320e2591707e480744dc8fc62bdf0
Billy McGee <61711986+wmcgee3@users.noreply.github.com>
2024-01-31 22:08:54 -0500

    fix markdown

--- z_osmf/src/lib.rs
+++ z_osmf/src/lib.rs
@@ -2,11 +2,11 @@
 
 //! # z_osmf
 //!
-//! The VERY work in progress Rust z/OSMF :tm: [^1] Client.
+//! The VERY work in progress Rust z/OSMF<sup>TM</sup> [^1] Client.
 //!
 //! ---
 //!
-//! [^1]: z/OSMF :tm:, z/OS :tm:, and the lowercase letter z :tm: (probably) are trademarks owned by International Business Machines Corporation ("IBM").
+//! [^1]: z/OSMF<sup>TM</sup>, z/OS<sup>TM</sup>, and the lowercase letter z<sup>TM</sup> (probably) are trademarks owned by International Business Machines Corporation ("IBM").
 //! This crate is not approved, endorsed, acknowledged, or even tolerated by IBM.
 //! (Please don't sue me, Big Blue)
 
